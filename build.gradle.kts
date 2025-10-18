0c5b88cdc9c4a00c3a8156d2f0da70c6c2640f84
ihateblueb <ihateblueb@proton.me>
2025-10-18 12:38:57 -0400

    routing!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
 	kotlin("jvm") version "2.2.0"
+	id("io.ktor.plugin") version "3.3.1"
 }
 
 group = "site.remlit.blueb"
-version = "1.0-SNAPSHOT"
+version = "1.2-SNAPSHOT"
 
 repositories {
 	mavenCentral()
-	maven {
-		url = uri("https://repo.remlit.site/releases")
-	}
 	maven {
 		url = uri("https://repo.remlit.site/snapshots")
 	}
@@ -17,7 +17,18 @@ repositories {
 
 dependencies {
 	// Only needed for compilation, all this code will already be present when run by Aster
-	compileOnly("site.remlit.blueb:aster:2025.9.1.0-SNAPSHOT")
+	compileOnly("site.remlit.blueb:aster:2025.10.2.0-SNAPSHOT")
+
+	// Most dependencies will already be used in Aster.
+	// You can choose to include them with implementation, but it's recommended you don't
+	compileOnly("ch.qos.logback:logback-classic:1.5.18")
+	compileOnly("org.slf4j:slf4j-api:2.0.17")
+	compileOnly("io.ktor:ktor-server-core:3.3.1")
+}
+
+// Prevent Ktor plugin from trying to make a jar for a standalone application
+tasks.withType<ShadowJar> {
+	enabled = false
 }
 
 kotlin {
--- src/main/kotlin/TestPlugin.kt
+++ src/main/kotlin/TestPlugin.kt
@@ -1,21 +1,33 @@
 package site.remlit.blueb.asterTestPlugin
 
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import site.remlit.blueb.aster.event.EventRegistry
 import site.remlit.blueb.aster.event.note.NoteCreateEvent
 import site.remlit.blueb.aster.plugin.AsterPlugin
+import site.remlit.blueb.aster.route.RouteRegistry
 
 @Suppress("Unused")
 class TestPlugin : AsterPlugin {
-	override fun enable() {
-		println("tp: Plugin enabled!")
+	override val logger: Logger =
+		LoggerFactory.getLogger(this::class.java)
 
+	override fun enable() {
 		EventRegistry.addListener(NoteCreateEvent::class) {
 			it as NoteCreateEvent
-			println("tp: Received note create event: ${it.note}")
+			logger.info("Received note create event: ${it.note}")
+		}
+
+		RouteRegistry.registerRoute {
+			get("/test-plugin-route") {
+				call.respondText {
+					"TestPlugin route works!"
+				}
+			}
 		}
 	}
 
-	override fun disable() {
-		println("tp: Plugin disabled!")
-	}
+	override fun disable() { }
 }
